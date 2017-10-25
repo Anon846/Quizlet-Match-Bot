bc97ab3979151ca0eb669ae5bb48fb4db9b59193
sirthrowdoe <sirthrowdoe@gmail.com>
2017-10-24 21:08:53 -0700

    Add files via upload

--- bot1.py
+++ bot1.py
@@ -4,3 +4,4 @@ from getpass import getpass
 driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
 driver.get("https://quizlet.com/")
 
+login = driver.find_element_by_xpath("//*[@id='SiteHeaderReactTarget']/header/div[1]/div/span[2]/div[2]/div/button[1]")
