b2b31326d32bfd634ad452133ddb20a3161565cc
sirthrowdoe <sirthrowdoe@gmail.com>
2017-10-27 18:47:23 -0700

    Add files via upload

--- bot1.py
+++ bot1.py
@@ -1,8 +1,22 @@
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from getpass import getpass
+import getpass
 
+#Block of login code
+username = raw_input("Username: ")
+password = getpass.getpass()
 driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
 driver.get("https://quizlet.com/")
+login_button_1 = driver.find_element_by_css_selector(".SiteHeader-signIn>button")
+login_button_1.click()
+username_field = driver.find_element_by_css_selector(".LoginPromptModal-loginFieldsWrapper>.UIInput>.UIInput-content>.UIInput-input")
+username_field.send_keys(username)
+password_field = driver.find_element_by_css_selector(".LoginPromptModal-loginFieldsWrapper>.UIInput:nth-child(2)>.UIInput-content>.UIInput-input")
+password_field.send_keys(password)
+login_button_2 = driver.find_element_by_css_selector(".LoginPromptModal-form>.UIButton--hero")
+login_button_2.click()
+# End of login code
 
-login = driver.find_element_by_css_selector(".SiteHeader-signIn>button")
-login.click()
+
+
